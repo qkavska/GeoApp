ec52c2677f43f5fd2089ea157575055ee429b37b
qkavska <qkavska@gmail.com>
2023-07-07 20:31:01 +0200

    po kliknięciu w przyciski dane pojawiają się na profilu

--- script.js
+++ script.js
@@ -7,62 +7,124 @@ let pipButton = document.getElementById("pip");
 let piButton = document.getElementById("pi");
 let pgButton = document.getElementById("pg");
 let gButton = document.getElementById("g");
+let zlButton = document.getElementById("zl");
+let brButton = document.getElementById("br");
+let szButton = document.getElementById("sz");
+let czButton = document.getElementById("cz");
+let halfButton = document.getElementById("half");
+let oneButton = document.getElementById("one");
+let twoButton = document.getElementById("two");
+let threeButton = document.getElementById("three");
+let fourButton = document.getElementById("four");
+let fiveButton = document.getElementById("five");
+let sixButton = document.getElementById("six");
+let deleteButton = document.getElementById("delete");
 
 let buttons = document.getElementsByClassName("but");
 let myprofil = document.getElementById("my-profil")
+let dataContainer = document.createElement("div");
+myprofil.appendChild(dataContainer);
+let data = []
 
 zButton.addEventListener ("click", function() {
-  let buttonText = zButton.textContent
-  placeholder.value = buttonText;});
+let buttonText = zButton.textContent;
+myprofil.value = buttonText;
+});
 
-prButton.addEventListener ("click", function() {
-    let buttonText = prButton.textContent
+  prButton.addEventListener ("click", function() {
+    let buttonText = prButton.textContent;
   
-    placeholder.value = buttonText;
+    myprofil.value = buttonText;
   });
   
   psButton.addEventListener ("click", function() {
-    let buttonText = psButton.textContent
+    let buttonText = psButton.textContent;
   
-    placeholder.value = buttonText;
+    myprofil.value = buttonText;
   });
   
   pdButton.addEventListener ("click", function() {
-    let buttonText = pdButton.textContent
+    let buttonText = pdButton.textContent;
   
-    placeholder.value = buttonText;
+    myprofil.value = buttonText;
   });
   
-      
   pipButton.addEventListener ("click", function() {
-    let buttonText = pipButton.textContent
+    let buttonText = pipButton.textContent;
   
-    placeholder.value = buttonText;
+    myprofil.value = buttonText;
   });
   
   piButton.addEventListener ("click", function() {
-      let buttonText = piButton.textContent
+      let buttonText = piButton.textContent;
     
-      placeholder.value = buttonText;
-    });
+      myprofil.value = buttonText;
+  });
     
-    pgButton.addEventListener ("click", function() {
-      let buttonText = pgButton.textContent
+  pgButton.addEventListener ("click", function() {
+      let buttonText = pgButton.textContent;
     
-      placeholder.value = buttonText;
-    });
+      myprofil.value = buttonText;
+  });
     
-    gButton.addEventListener ("click", function() {
-      let buttonText = gButton.textContent
+  gButton.addEventListener ("click", function() {
+      let buttonText = gButton.textContent;
     
-      placeholder.value = buttonText;
-    });
+      myprofil.value = buttonText;
+  });
+
+  zlButton.addEventListener ("click", function() {
+      let buttonText = zlButton.textContent;
+    
+      myprofil.value = buttonText;
+  });
+    
+  brButton.addEventListener ("click", function() {
+      let buttonText = brButton.textContent
+    
+      myprofil.value = buttonText;
+  });
+    
+  szButton.addEventListener ("click", function() {
+      let buttonText = szButton.textContent;
+    
+      myprofil.value = buttonText;
+  });
+
+  czButton.addEventListener ("click", function() {
+      let buttonText = czButton.textContent;
+    
+      myprofil.value = buttonText;
+  });
+
 
     for (let i = 0; i < buttons.length; i++){
       buttons[i].addEventListener("click", function(){
         let buttonData = this.textContent;
-        let listItem = document.createElement("li");
-        listItem.textContent = buttonData;
-        myprofil.appendChild(listItem);
+        data.push(buttonData);
+
+        displayData();
+  
       });
     }
+
+    // function displayData(){
+    //   dataContainer.innerHTML = "";
+
+    //   for (let i = 0; i < data.length; i++) {
+    //     let dataElement = document.createElement("div");
+    //     let dataText = document.createElement("button");
+    //     deleteButton.dataset.index = i;
+    //     deleteButton.addEventListener("click", removeData);
+    //     dataElement.appendChild(dataText);
+    //     dataElement.appendChild(deleteButton);
+    //     dataContainer.appendChild(dataElement);
+    //     myprofil.appendChild(dataElement);
+    //   }
+    // }
+
+    // function removeData(event) {
+    //   let dataIndex = event.target.dataset.index();
+    //   data.splice(dataindex, 1);
+    //   displayData();
+    // }
